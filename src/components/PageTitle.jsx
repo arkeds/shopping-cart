1043bf9f9280a657de5d6f44b64a5b7afb1db49c
Arvin Kenn <adsantos.417@gmail.com>
2021-08-14 15:17:05 +0800

    Added Products Page

---
+++ src/components/PageTitle.jsx
@@ -0,0 +1,9 @@
+const PageTitle = ({ title = 'Home' }) => {
+  return (
+    <div>
+      <h1 className='text-4xl font-bold text-center text-center'>{title}</h1>
+    </div>
+  )
+}
+
+export default PageTitle
---
+++ src/helpers/products.js
@@ -0,0 +1,16 @@
+import faker from 'faker'
+
+export const fakeProducts = (total) => {
+  let products = []
+  for (let index = 0; index < total; index++) {
+    products.push({
+      id: faker.datatype.uuid(),
+      name: faker.commerce.product(),
+      department: faker.commerce.department(),
+      image: faker.image.fashion(),
+      price: faker.commerce.price(),
+      stock: Math.floor(Math.random() * 100 + 10),
+    })
+  }
+  return products
+}
